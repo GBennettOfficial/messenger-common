f658774f595fd34b5c2e7af344e2ddb30b21570f
GeorgeBennett <GBennettOfficial@outlook.com>
2025-05-29 18:45:04 -0400

    save

--- Messenger.Common/RegisterDto.cs
+++ Messenger.Common/RegisterDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace Messenger.Common;
 
-public record RegisterDto(string Username, string FirstName, string LastName, string Email, string? Phone, string? Picture, string Password);
+public record RegisterDto(string Username, string Email, string Password);
